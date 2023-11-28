e5f97a0d4b21c37370f3b700d0b7f70676af798a
lostmostcost <lmc2151@icloud.com>
2023-11-28 10:38:32 +0000

    ssh 3-4

--- pybo/views.py
+++ pybo/views.py
@@ -4,11 +4,14 @@ from django.utils import timezone
 
 from .forms import QuestionForm, AnswerForm
 from .models import Question
-
+from django.core.paginator import Paginator
 
 def index(request):
+    page = request.GET.get('page', '1')
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 
